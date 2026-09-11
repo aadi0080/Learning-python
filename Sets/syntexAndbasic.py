631dd16ac8daa2b505251f9c2559529fe913adea
aadi0080 <aadarshjaiswal9696@gmail.com>
2026-09-11 14:00:26 +0530

    syntex  of sets

---
+++ Sets/syntexAndbasic.py
@@ -0,0 +1,5 @@
+collection = {1 ,3 , 5 , 9 , 34  , 45 , 67 ,5 , 1 , 3 ,  89 , 90 , 100}
+# set  is  a  of unique and immutable data type  
+# but  set is mutable  but  element of  set is immutable  
+print(collection)
+# set  never  return duplicate value and it  is  unorderd 
